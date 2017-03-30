109999ae7bc2e00d30a730f6275f6226289e1b42
christopher <christoipher.chen@gmail.com>
2017-03-29 21:35:56 -0700

    Refactor for packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='mod_pydoc',
+      author='aroberge',
+      author_email='',
+      version='0.1.1',
+      description="""Prettier pydoc.""",
+      long_description="",
+      url='https://github.com/aroberge/mod_pydoc',
+      packages=['mod_pydoc'],
+      install_requires=[],
+      scripts=["bin/mod_pydoc"],
+      )
